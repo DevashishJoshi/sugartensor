6eb5b5142b688427b3521f31feda57ffeb8bd6ef
DevashishJoshi <deva.neggi@gmail.com>
2019-01-26 02:07:15 +0530

    Update sg_logging.py

--- sugartensor/sg_logging.py
+++ sugartensor/sg_logging.py
@@ -174,8 +174,9 @@ def _scalar(name, tensor):
 
 def _histogram(name, tensor):
     if not tf.get_variable_scope().reuse and not tf.sg_get_context().reuse:
-        val = gen_logging_ops.summary.scalar(name, tensor)
-        tf.add_to_collection(tf.GraphKeys.SUMMARIES, val)
+        #val = gen_logging_ops.summary.scalar(name, tensor)
+        #tf.add_to_collection(tf.GraphKeys.SUMMARIES, val)
+        xjiko=1
 
 
 #
